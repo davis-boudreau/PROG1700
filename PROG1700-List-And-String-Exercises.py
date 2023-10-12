0eb4e4b6709842e452419ba639c09535dd2c9953
davis-boudreau <146293966+davis-boudreau@users.noreply.github.com>
2023-10-12 14:59:19 -0300

    Fist step to for loops

---
+++ PROG1700-List-And-String-Exercises.py
@@ -0,0 +1,23 @@
+"""
+PROG1700
+List and String Exercise
+
+You are given a list of animal names in a list data structure: 
+names = ["Mike", "Harper", "Tike", "Carter", "Porter", "Parker"]
+
+Working backwards through the list, print each person’s name backwards like so:
+rekraP
+retroP
+retraC
+ekiT
+repraH
+ekiM
+"""
+# Declare a list
+names = ["Mike", "Harper", "Tike", "Carter", "Porter", "Parker"]
+for name in names:
+    print(name)
+
+
+
+
